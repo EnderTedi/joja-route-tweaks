9ca5d64eeae38c13bd19a398fcdb77b3ed0c3014
EnderTedi <150543513+EnderTedi@users.noreply.github.com>
2026-06-20 04:08:35 +0200

    Fix positions of movie posters (I forgot they existed) Fix Joja Theater Build farm event still triggering

--- JojaTheaterTweaks/Patches/MoveMovieTheater.cs
+++ JojaTheaterTweaks/Patches/MoveMovieTheater.cs
@@ -1,5 +1,6 @@
 using HarmonyLib;
 using JetBrains.Annotations;
+using Microsoft.Xna.Framework;
 using StardewValley;
 using StardewValley.Events;
 using StardewValley.Locations;
@@ -9,6 +10,7 @@ using System.Reflection.Emit;
 namespace JojaTheaterTweaks.Patches;
 
 [UsedImplicitly, HarmonyPatch(typeof(Town))]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
 public class MoveMovieTheater
 {
     [HarmonyPatch(nameof(Town.MakeMapModifications)), HarmonyTranspiler]
@@ -28,13 +30,19 @@ public class MoveMovieTheater
 
         return matcher.Instructions();
     }
+
+    [HarmonyPatch(nameof(Town.GetTheaterTileOffset)), HarmonyPostfix]
+    public static void MoviePosterPositions(ref Point __result)
+    {
+        __result = new(0, 0);
+    }
 }
 
 [UsedImplicitly, HarmonyPatch(typeof(Utility)), SuppressMessage("ReSharper", "InconsistentNaming")]
 public class NoFixingTheWarehouseJustToNotChangeIt
 {
     [HarmonyPatch(nameof(Utility.pickFarmEvent)), HarmonyPostfix]
-    public static void RemoveFarmEvent(FarmEvent? __result)
+    public static void RemoveFarmEvent(ref FarmEvent? __result)
     {
         if (__result is WorldChangeEvent wce && wce.whichEvent.Value == 10)
             __result = null;
